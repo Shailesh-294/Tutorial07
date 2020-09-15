da0a956db0fb5eb6c892c03c396f9104dc2cd4e6
Shailesh-294 <slakhani062@rku.ac.in>
2020-09-15 15:52:03 +0530

    AlertDialog box added in welcome.java on click logout

--- app/src/main/java/com/example/tutorial07/Welcome.java
+++ app/src/main/java/com/example/tutorial07/Welcome.java
@@ -1,13 +1,16 @@
 package com.example.tutorial07;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
@@ -15,6 +18,7 @@ public class Welcome extends AppCompatActivity {
     TextView welcome;
     SharedPreferences share;
     SharedPreferences.Editor editor;
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,7 @@ public class Welcome extends AppCompatActivity {
 
         share = getSharedPreferences("Ldata",MODE_PRIVATE);
         editor = share.edit();
+        builder = new AlertDialog.Builder(Welcome.this);
         String user = share.getString("username","");
         welcome = findViewById(R.id.welcome);
         welcome.setText("Welcome "+user);
@@ -39,12 +44,22 @@ public class Welcome extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.logout){
-            editor.putBoolean("login",false);
-            editor.commit();
-            Intent intent = new Intent(Welcome.this,Login.class);
-            startActivity(intent);
-            finish();
-        }
+            builder.setTitle("Logout")
+                    .setCancelable(false)
+                    .setMessage("Are you sure want to logout ?")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            editor.putBoolean("login",false);
+                            editor.commit();
+                            Intent intent = new Intent(Welcome.this,Login.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }).setNegativeButton("Cancle",null);
+            AlertDialog alert = builder.create();
+            alert.show();
+            }
         return true;
     }
 }
